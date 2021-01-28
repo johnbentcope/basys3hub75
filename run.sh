cd3f0329427153142713d0e63faa7d98ea82f4b8
John Cope <johnbentcope@gmail.com>
2021-01-27 21:32:28 -0500

    improving sythesizability

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 yosys run_yosys.ys
 vivado -nolog -nojournal -mode batch -source run_vivado.tcl
 vivado -nolog -nojournal -mode batch -source run_prog.tcl
+tput bel
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 iverilog $1*.v -g2012 -o $1.vvp
 vvp $1.vvp -lxt2
 #gtkwave $1.gtkw
+tput bel
